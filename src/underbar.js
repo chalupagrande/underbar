b719fee26a6d182e211ca12ee9913d27f7b94933
Jamie Skinner <skinner520@gmail.com>
2015-02-09 02:43:22 -0600

    finished Part 1 - solve _.reject w/o using _.filter

--- src/underbar.js
+++ src/underbar.js
@@ -94,6 +94,13 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var result = [];
+    _.each(collection, function(item, index){
+      if(!test(item)){
+        result.push(item);
+      }
+    })
+    return result;
 
   };
 
